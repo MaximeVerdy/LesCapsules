26c430172e4ea524ee94af3960f1801bd6986b1c
Maxime Verdy <maximeverdy@gmail.com>
2021-02-20 17:57:28 +0100

    modif package.json

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'reactapp/build')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
